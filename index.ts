0ed51656072db408b4df681041fd64349449f040
Zhani Baramidze <zhani.baramidze@bolt.eu>
2020-05-24 15:33:02 +0300

    changed mongo addr

--- index.ts
+++ index.ts
@@ -11,10 +11,12 @@ export const MAX_TIME_IN_ACTION_NAME_SEC = 60;
 export const MAX_TIME_IN_ACTION_CHOOSE_SEC = 60;
 export const MAX_TIME_IN_ACTION_SCORES_SEC = 20;
 
-mongoose.connect("mongodb://localhost:27099/drawful", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://admin:ojs763985jsnb64a@cluster0-pjozz.mongodb.net/drawful", {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(3000, () => console.log(`Listening at http://localhost:${3000}`));
+    })
     .catch((e) => {
         console.log("ERROR! Failed connecting to mongoose" + JSON.stringify(e));
     });
 
-app.listen(3000, () => console.log(`Listening at http://localhost:${3000}`));
 
