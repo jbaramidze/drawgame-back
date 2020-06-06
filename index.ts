b18c796f96c4f941376f8eb9ac529bbb4da329cc
Zhani Baramidze <zhani.baramidze@bolt.eu>
2020-06-06 10:33:47 +0300

    Fixes of testing

--- index.ts
+++ index.ts
@@ -10,12 +10,14 @@ export const POINTS_FOR_MISLEADING_SOMEONE = 1;
 export const MAX_TIME_IN_ACTION_NAME_SEC = 60;
 export const MAX_TIME_IN_ACTION_CHOOSE_SEC = 60;
 export const MAX_TIME_IN_ACTION_SCORES_SEC = 20;
-mongoose.connect(`${process.env.MONGO_ADDR}/drawgame`, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        app.listen(process.env.LISTEN_PORT, () => console.log(`Listening at http://localhost:${3000}`));
-    })
-    .catch((e) => {
-        console.log("ERROR! Failed connecting to mongoose" + JSON.stringify(e));
-    });
-
-
+if (process.env.is_test) {
+    app.listen(3001, () => console.log(`Listening at http://localhost:${3010}`));
+} else {
+    mongoose.connect(`${process.env.MONGO_ADDR}/drawgame`, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => {
+            app.listen(3000, () => console.log(`Listening at http://localhost:${3000}`));
+        })
+        .catch((e) => {
+            console.log("ERROR! Failed connecting to mongoose" + JSON.stringify(e));
+        });
+}
